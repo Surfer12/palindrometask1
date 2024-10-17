1b50720d9c4e29877101ea1eefd6a15f53e80e81
dakinestyle <ryanoatsie@outlook.com>
2024-10-16 19:39:58 -0700

    Update project settings and folder structure

--- src/solutionTwo.java
+++ src/solutionTwo.java
@@ -1,3 +1,17 @@
+// ValidPalindromeIIPrompt.md
+
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+// Example 1:
+// Input: s = "aba"
+// Output: true
+
+// Example 2:
+// Input: s = "abca"
+
+
+
 public class solutionTwo {
+
 
 }
