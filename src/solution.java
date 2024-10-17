1efe0142a191ce2ed526376c57b2d59d95ae376c
Ryan Oates <ryanoatsie@outlook.com>
2024-10-16 20:41:20 -0700

    Refactor solutionTwo class to include a method for checking if a string is a valid palindrome

--- src/solution.java
+++ src/solution.java
@@ -1,28 +1,31 @@
-/* 680. Valid Palindrome II
-Easy
+/* A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
-Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
 
 Example 1:
-Input: s = "aba"
-Output: true
 
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
 Example 2:
-Input: s = "abca"
-Output: true
-Explanation: You could delete the character 'c'.
 
-Example 3:
-Input: s = "abc"
+Input: s = "race a car"
 Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
 
-Example 4:
 Input: s = " "
 Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
 
 Constraints:
-- 1 <= s.length <= 10^5
-- s consists of lowercase English letters. */
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters. */
 
 public class solution{
 
